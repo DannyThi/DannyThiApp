61a6b5a4ef972453d074e7349c8494e7016d1560
Danny Thi <hailongdannythi@gmail.com>
2020-12-09 19:52:52 +0900

    Initial Commit.

---
+++ DannyThiApp/DannyThiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DannyThiAppApp.swift
+//  DannyThiApp
+//
+//  Created by Hai Long Danny Thi on 2020/12/09.
+//
+
+import SwiftUI
+
+@main
+struct DannyThiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
