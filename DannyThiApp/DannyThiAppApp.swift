b51b273c3fe1368c1334aaf9bc778c4ce5e4392a
Danny Thi <hailongdannythi@gmail.com>
2020-12-16 10:58:42 +0900

    Added Core data and CloudKit.

--- DannyThiApp/DannyThiAppApp.swift
+++ DannyThiApp/DannyThiAppApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct DannyThiAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+   @StateObject var dataController: DataController
+   
+   var body: some Scene {
+      WindowGroup {
+         ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+      }
+   }
+   
+   init() {
+      let dataController = DataController()
+      _dataController = StateObject(wrappedValue: dataController)
+   }
 }
