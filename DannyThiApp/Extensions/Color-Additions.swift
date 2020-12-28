c73e8708442d10d79263a5f1ba880e438b8d363b
Danny Thi <hailongdannythi@gmail.com>
2020-12-28 21:45:04 +0900

    Home view.

---
+++ DannyThiApp/Extensions/Color-Additions.swift
@@ -0,0 +1,15 @@
+//
+//  Color-Additions.swift
+//  DannyThiApp
+//
+//  Created by Hai Long Danny Thi on 2020/12/22.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+extension Color {
+   static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+   static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
